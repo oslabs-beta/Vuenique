64e7f0f72b59f01e0661d317fe0240a013360eff
tmg2549 <tmg2549@utexas.edu>
2022-04-07 15:49:54 -0500

    Refine unit tests for Bar
    Removed innerRef prop becuase forwarding refs are not a common pattern
    for Vue and did not deem them necessary. Added another unit test and
    cleaned up file and comments for Bar and Bar test.

--- packages/vuez-shape/Bar.vue
+++ packages/vuez-shape/Bar.vue
@@ -1,41 +1,19 @@
-<!-- may need to move this to where the svg element lives later -->
-<script lang="ts">
-export default {
-  // Prevent the root element from inheriting the fallthrough attributes if set false
-  inheritAttrs: true,
-};
-</script>
-
 <script setup lang="ts">
-import { onMounted, type Ref, defineExpose, ref } from "vue";
+import { type Ref } from "vue";
 
 // Type declarations for the <rect> tag
 interface BarProps {
   // render class attributes for <rect> tag
   class?: string;
-  // reference to underlying <rect> tag
-  innerRef?: Ref<SVGRectElement>;
 }
 
 const barProps = defineProps<BarProps>();
-// const rect = ref<HTMLElement | null>(null);
-
-onMounted(() => {
-  barProps.innerRef?.value.focus();
-  // barProps.innerRef?.value?.focus();
-  console.log("barProps.innerRef", barProps.innerRef);
-  // console.log("rect", rect.value?.getBoundingClientRect());
-});
 </script>
 
 <template>
   <!-- $attrs handle additional props not explicity defined in the interface BarProps -->
-  <input ref="el" type="text" />
-
   <rect
-    ref="barProps.innerRef"
     :class="barProps.class"
     v-bind="$attrs"
   />
-
 </template>
--- packages/vuez-shape/__tests__/Bar.spec.ts
+++ packages/vuez-shape/__tests__/Bar.spec.ts
@@ -1,10 +1,9 @@
-// import { render } from '@vue/server-test-utils';
-import {ref} from 'vue';
-import { describe, it, expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { mount } from "@vue/test-utils";
 
 import Bar from "../Bar.vue";
 
+// Creates wrapper component around Bar for testing
 const BarWrapper  = (restProps = {}, attrs = {}) => mount(Bar, {
     shallow: true,
     propsData: {
@@ -14,54 +13,27 @@ const BarWrapper  = (restProps = {}, attrs = {}) => mount(Bar, {
       ...attrs,
     },
   });
-const TestBar = BarWrapper({class: 'test'}, {x:50})
-console.log(TestBar.props());
 
-console.log(TestBar.vm.$attrs);
-
-describe('Bar component', () => {
-  test('Bar should be defined', () => {
+describe("Bar component...", () => {
+  test("should be defined", () => {
     expect(Bar).toBeDefined();
   });
 
-  test('it should have test class', () => {
+  test("should have test class passed in through props", () => {
     expect(
       BarWrapper({
-        class: 'test',
-      }).props('class'),
-    ).toBe('test');
+        class: "test",
+      }).props("class")
+    ).toBe("test");
   });
 
-  test('Bar should expose its ref via innerRef attribute', async () => {
-    // console.log(ref);
-    const fakeRef = ref<SVGRectElement>();
-    //console.log('fakeRef', fakeRef);
-    const wrapper = BarWrapper({innerRef: fakeRef, class: "test"});
-    console.log('wrapper', wrapper.vm.innerRef)
-    // const element = wrapper.find('.test')
-    // console.log('element', element);
-    // console.log('refs: ', wrapper.vm.$refs);
-    // console.log('barComponent props', wrapper.props());
-    const refComponent = wrapper.find("rect");
-    const refString = "barProps.innerRef";
-    const refsObject = wrapper.find({ref: refString});
-    // console.log("$refs", wrapper.vm.$refs);
-    console.log("refComponent", refComponent.element);
-    console.log("refsObject", refsObject);
-    // console.log('refComponent', refComponent)
-    expect(refsObject.element as SVGRectElement).toBe(refComponent.element as SVGRectElement);
-    // expect(refsObject).toContainElement(refComponent.element);
+  test("should have additional attributes passed in through attrs", () => {
+    expect(
+      BarWrapper({}, {
+          x: 10,
+          y: 10,
+        }
+      ).vm.$attrs
+    ).toEqual({ x: 10, y: 10 });
   });
 });
-
-//   test('it should expose its ref via an innerRef prop', () => {
-//     const fakeRef = React.createRef<SVGRectElement>();
-//     const { container } = render(
-//       <svg>
-//         <Bar innerRef={fakeRef} />
-//       </svg>,
-//     );
-//     const RectElement = container.querySelector('rect');
-//     expect(fakeRef.current).toContainElement(RectElement);
-//   });
-// });
