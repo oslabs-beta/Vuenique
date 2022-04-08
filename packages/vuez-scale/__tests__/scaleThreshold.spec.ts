9ae06c95df844fd0d1c74a89b5805bc42106951a
MiaowZ <zmw0525@gmail.com>
2022-04-08 18:13:36 -0400

    Set up unit tests for log.ts, ordinal.ts, quantile.ts, threshold.ts, and utc.ts

---
+++ packages/vuez-scale/__tests__/scaleThreshold.spec.ts
@@ -0,0 +1,20 @@
+import { scaleThreshold } from '../';
+
+describe('scaleThreshold', () => {
+
+  test('should be defined', () => {
+    expect(scaleThreshold).toBeDefined();
+  });
+
+  test('set domain', () => {
+    const domain = [0, 350];
+    const scale = scaleThreshold({ domain });
+    expect(scale.domain()).toEqual(domain);
+  });
+
+  test('set range', () => {
+    const range = [2, 3];
+    const scale = scaleThreshold({ range });
+    expect(scale.range()).toEqual(range);
+  });
+});
