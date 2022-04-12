5d5cc258257d986f1507c529ac6acad05cd261b0
James Ma <james.shny1721@gmail.com>
2022-04-11 20:37:35 -0700

    run reconfigured vite build

---
+++ packages/index.js
@@ -0,0 +1,6 @@
+export { default as Group } from './vuenique-group/Group.vue';
+export { default as Bar } from './vuenique-shape/Bar.vue';
+export { default as Circle } from './vuenique-shape/Circle.vue';
+export { default as LinePath } from './vuenique-shape/LinePath.vue';
+
+export * from './vuenique-scale/index.ts'
