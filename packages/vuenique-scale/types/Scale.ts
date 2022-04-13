1f3527e1a8477c1ea28769e0ad9000f9a1e98be6
MiaowZ <zmw0525@gmail.com>
2022-04-13 19:45:05 -0400

    Delete types in scale

--- packages/vuenique-scale/types/Scale.ts
+++
@@ -1,7 +0,0 @@
-// export type D3Scale<
-//   Output = DefaultOutput,
-//   DiscreteInput extends StringLike = StringLike,
-//   ThresholdInput extends DefaultThresholdInput = DefaultThresholdInput,
-// > = ValueOf<ScaleTypeToD3Scale<Output, DiscreteInput, ThresholdInput>>;
-
-
